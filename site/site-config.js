56b7643730bb688e33e81184b48377db47d38d9d
MacBook Pro <macbookpro@MacBooks-MBP.lan>
2026-09-08 11:00:00 +0530

    Add business contact details

--- site/site-config.js
+++ site/site-config.js
@@ -1,5 +1,5 @@
 // Replace these values before deploying the public website.
 const SITE_CONFIG = {
-  whatsappBookingUrl: 'https://wa.me/919876543210',
-  supportEmail: 'support@example.com'
+  whatsappBookingUrl: 'https://wa.me/919582713551',
+  supportEmail: 'admin@metrotooffice.in'
 };
